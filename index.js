9839c43633285bfd1a156fb59f0cfedcdb532c38
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-22 20:26:25 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,63 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000;
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const jwt = require('jsonwebtoken');
 
-// MongoDB URI and other sensitive info should be in environment variables
-const uri = process.env.MONGODB_URI || "mongodb+srv://zhikangsam0724:2Un24f6Hfk4l1Z1x@cluster0.1jh2xph.mongodb.net/";
-
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    // ... other options ...
-  },
-  apis: ['C:/Users/PC/Desktop/BENR 3433 Information Security/assignment/BENR3433-S2G9/index.js'], // Path to the API docs
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// MongoDB Client Setup
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-// Connect to MongoDB
-async function connectToMongo() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-  } catch (e) {
-    console.error("Failed to connect to MongoDB", e);
-  }
-}
-
-// Call the function to connect to MongoDB
-connectToMongo();
-
-app.use(express.json());
-
-// Define your routes and handlers here
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!');
-});
+   res.send('Hello World!')
+})
 
-// Define other functions like login, register, etc.
-
-// Ensure you have error handling for routes and database operations
-
-// Start the Express server
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+   console.log(`Example app listening on port ${port}`)
+})
 
 
 
