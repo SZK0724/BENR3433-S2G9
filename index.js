495f08acecdc440d4b2624f851db82e25672ca7c
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-12-22 20:29:48 +0800

    Your commit message here

--- index.js
+++ index.js
@@ -13,6 +13,40 @@ app.listen(port, () => {
 })
 
 
+/**
+ * @swagger
+ * /register/user:
+ *   post:
+ *     summary: Register a new user
+ *     description: Allows a new user to create an account.
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *               - name
+ *               - email
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Account created successfully.
+ *       400:
+ *         description: Bad request.
+ */
+
 
 
 
